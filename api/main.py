b174e542af949820369ca7bbc4367b3e507a9686
juhis <jkarjala@broadinstitute.org>
2026-05-24 10:59:40 +0000

    use root logger

--- api/main.py
+++ api/main.py
@@ -527,4 +527,4 @@ async def get_stats():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")), log_config=None)
